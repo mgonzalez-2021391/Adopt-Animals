15f6a975931454ba939a7e9c366bc897d37b47d6
mgonzalez-2021391 <mgonzalez-2021391@kinal.edu.gt>
2024-03-03 20:02:51 -0600

    Revocación de AdoptionSytem antiguo

---
+++ AdoptionSystem/src/utils/validator.js
@@ -0,0 +1,56 @@
+//Validar diferentes datos
+'use strict'
+
+import { compare, hash } from 'bcrypt'
+
+//Encriptar la contraseña
+export const encrypt = async (password) => {
+    try {
+        return hash(password, 10)
+    } catch (err) {
+        console.error(err)
+        return err
+    }
+}
+
+//Validar la contraseña
+export const checkPassword = async (password, hash) => {
+    try {
+        return compare(password, hash)
+    } catch (err) {
+        console.error(err)
+        return err
+    }
+}
+
+export const checkUpdate = (data, userId) => {
+    if (userId) {
+        if (
+            Object.entries(data).length === 0 ||
+            data.password ||
+            data.password == '' ||
+            data.role ||
+            data.role == ''
+        ) {
+            return false
+        }
+        return true
+    } else {
+        return false
+    }
+}
+
+export const checkUpdateAnimal = (data, animalId) => {
+    if (animalId) {
+        if (
+            Object.entries(data).length === 0 ||
+            data.keeper ||
+            data.keeper == ''
+        ) {
+            return false
+        }
+        return true
+    } else {
+        return false
+    }
+}
